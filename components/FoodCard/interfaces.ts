c9af3d85e8eca76f09a5e16e597e7a13e9e373bc
ftr9 <ferlandodotel16@gmail.com>
2023-09-16 14:10:58 +0545

    Feat: Added types

---
+++ components/FoodCard/interfaces.ts
@@ -0,0 +1,9 @@
+export interface IFoods {
+  name: string;
+  price: number;
+  image: any;
+  inputXRange: number[];
+  outputYRange: number[];
+  ouputRotationRange: number[];
+  outputTextOpacityRange: number[];
+}
