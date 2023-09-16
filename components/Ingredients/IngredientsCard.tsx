26c757f393a3dc554f3ad42c07daa4646291d253
ftr9 <ferlandodotel16@gmail.com>
2023-09-16 13:11:51 +0545

    Feat: Added ingredients Card component

---
+++ components/Ingredients/IngredientsCard.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import Animated, { FadeInRight, FadeOutLeft } from 'react-native-reanimated';
+import { Checkbox } from 'expo-checkbox';
+import { IIngredientsWithSelectionStatus } from './types/interface';
+import useInredientsStore from './useIngredients.store';
+import SelectedImage from '../common/Images/SelectedImage';
+import PriceAndQuantity from '../common/GroupText/PriceAndQuantity';
+
+interface IIngredientsCardProps {
+  item: IIngredientsWithSelectionStatus;
+  index: number;
+}
+
+const IngredientsCard = ({ item, index }: IIngredientsCardProps) => {
+  const { selectIngredient, unselectedInredient } = useInredientsStore();
+
+  return (
+    <Animated.View
+      entering={FadeInRight.delay(index * 50).duration(index * 50)}
+      exiting={FadeOutLeft.delay(index * 50)}
+      key={item.name}
+      style={styles.ingredientCardContainer}
+    >
+      <SelectedImage uri={item.imageUri} />
+      <PriceAndQuantity name={item.name} quantity={item.quantity} />
+      <Checkbox
+        color={'black'}
+        style={{
+          height: 16,
+          width: 16,
+          backgroundColor: 'white',
+        }}
+        value={item.isSelected}
+        onValueChange={val => {
+          if (val) {
+            selectIngredient(item.name);
+          } else {
+            unselectedInredient(item.name);
+          }
+        }}
+      />
+    </Animated.View>
+  );
+};
+
+export default IngredientsCard;
+
+const styles = StyleSheet.create({
+  ingredientCardContainer: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
