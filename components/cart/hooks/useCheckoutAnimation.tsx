fb3405d1dcbde98d09676c4fc0b257be366589bf
ftr9 <ferlandodotel16@gmail.com>
2023-09-16 13:50:51 +0545

    Docs: Added documentation for animation in a custom useCheckoutAnimation hook

--- components/cart/hooks/useCheckoutAnimation.tsx
+++ components/cart/hooks/useCheckoutAnimation.tsx
@@ -18,18 +18,21 @@ const useCheckOutAnimation = () => {
   const SharedYPosValue = useSharedValue(0);
   const SharedOpacityValue = useSharedValue(1);
 
+  ////After the payment is completed increase the height of animateable container
   const AnimatedHeightStyle = useAnimatedStyle(() => {
     return {
       height: SharedHeightValue.value,
     };
   });
 
+  ////After the payment is completed move the bottom position of animateable container
   const AnimatedYPosStyle = useAnimatedStyle(() => {
     return {
       bottom: SharedYPosValue.value,
     };
   });
 
+  ////Used for blinking the button when payment is started
   const AnimatedOpacityStyle = useAnimatedStyle(() => {
     return {
       opacity: SharedOpacityValue.value,
@@ -38,6 +41,8 @@ const useCheckOutAnimation = () => {
 
   const checkOutClickHandle = async () => {
     setIsMakingPayment(true);
+
+    //// Start the blink animation of button
     SharedOpacityValue.value = withRepeat(
       withTiming(0, { duration: 500 }),
       -1,
@@ -46,24 +51,31 @@ const useCheckOutAnimation = () => {
 
     await new Promise(resolve => setTimeout(() => resolve(''), 3000));
 
+    ////After the payment is completed increase the height of animatable container
     const halfScreenHeight = (40 / 100) * SCREEN_HEIGHT;
     SharedYPosValue.value = withTiming(50, {
       duration: 800,
       easing: Easing.in(Easing.quad),
     });
+
+    ////At the same time move the animatable container from bottom 0 to halfScreenHeight value
     SharedHeightValue.value = withTiming(halfScreenHeight, {
       duration: 800,
       easing: Easing.in(Easing.quad),
     });
     await new Promise(resolve => setTimeout(() => resolve(''), 1000));
 
+    ////Cancel the blinking animation of button
     cancelAnimation(SharedOpacityValue);
+
     setIsCheckoutComplete(true);
     setIsMakingPayment(false);
   };
 
   const checkMarkBtnClickHandle = () => {
     setIsCheckoutComplete(false);
+
+    ////Reset the animated value of height and bottom to initial value
     SharedYPosValue.value = withTiming(0, {
       duration: 800,
       easing: Easing.in(Easing.quad),
@@ -72,6 +84,8 @@ const useCheckOutAnimation = () => {
       duration: 800,
       easing: Easing.in(Easing.quad),
     });
+
+    ////After the animation completed go back to main page
     setTimeout(() => {
       router.back();
     }, 1000);
